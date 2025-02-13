2e53ba7c101ca63030cd44e7718d927140ce27dc
shubhamrahate545 <140446330+shubhamrahate545@users.noreply.github.com>
2025-02-13 11:44:42 +0530

    Update app.py

--- Project/app.py
+++ Project/app.py
@@ -89,4 +89,6 @@ def generate_pdf():
        if os.path.exists(PDF_FILENAME):
            os.remove(PDF_FILENAME)
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use Render's provided port, default to 5000
+    app.run(host="0.0.0.0", port=port)
    app.run(debug=True)
